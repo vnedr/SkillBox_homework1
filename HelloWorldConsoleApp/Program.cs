51df193e001ca17583152fddf2f7f88505803cc5
Ivan.Sanin <v64@ya.ru>
2022-07-05 10:01:47 +0300

    3 - добавил в одно решение 2 задания

--- HelloWorldConsoleApp/Program.cs
+++ HelloWorldConsoleApp/Program.cs
@@ -1,5 +1,4 @@
-﻿
-// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more information
 using static System.Console;
 
 WriteLine("Hello World!!!");
---
+++ task_2/Program.cs
@@ -0,0 +1,8 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using static System.Console;
+
+Write("Hello ");
+Write("World");
+Write("!!!");
+ReadKey();
